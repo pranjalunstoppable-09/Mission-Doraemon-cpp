8b1ded0eb74d1ee9f64c6c8b0ea83c79ef83c9ae
Essu - TechPioneer <pranjalunstoppable@gmail.com>
2026-05-08 09:27:16 +0100

    "Added my first C++ program"
    "In this program, i explored how to transition from simple horizontal printing to 2D shape generation .
    
    1. Initialized logic with a single loop for a 1D line of stars.
    2. Implemented Nested For loops to control both rows (i) and coloumns (j)
    3. used variables 'n' and 'm' to make the pattern dyanamic , allowing for custom rectangle dimensions.
    
    this is a fundamental step in building the logic for coordinate-based systems in my project Doraemon."

---
+++ 5 stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	//for(int i=1; i<=5 ; i++) {
+	//	cout << " ***** " << endl;
+//	}
+
+int x=10;
+for(int i=1; i<=x ; i++) {
+	cout << " * " ;
+}
+	return 0;
+}
---
+++ stars .. 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main() {
+	int n = 6;
+	for(int i=1; i<=n ; i++) {
+		int m = 9;
+		for(int j=1 ; j<=m ; j++) {
+			cout << "*";
+		}
+		cout<< endl;;
+	}
+	
+	return 0;
+}
